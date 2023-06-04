297dfd8c60d24115f2a77b4ac3042e0a84b2cbfa
kidstone <hyuksu0308@naver.com>
2023-06-05 02:52:20 +0900

    views.py page 리턴 함수

--- server/noviceAngler/urls.py
+++ server/noviceAngler/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'noviceAngler'
 
 urlpatterns = [
-  # path("", views.main_page, name="main_page"),
+  path("", views.main_page, name="main_page"),
+  path("point_recommendation/", views.point_recommendation, name="point_recommendation"),
+  path("point_recommendation/question/", views.point_recommendation_question, name="point_recommendation_question"),
+  path("point_recommendation/result/", views.point_recommendation_result, name="point_recommendation_result"),
+  path("fish_information/", views.fish_information, name="fish_information"),
 ]
--- server/noviceAngler/views.py
+++ server/noviceAngler/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# def main_page(request, *args, **kwargs):
-#   return render(request, 'noviceAngler/main_page.html')
+
+def main_page(request, *args, **kwargs):
+  return render(request, 'noviceAngler/main_page.html')
+  
+def point_recommendation(request, *args, **kwargs):
+  return render(request, 'noviceAngler/point_recommendation.html')
+  
+def point_recommendation_question(request, *args, **kwargs):
+  return render(request, 'noviceAngler/point_recommendation_question.html')
+  
+def point_recommendation_result(request, *args, **kwargs):
+  return render(request, 'noviceAngler/point_recommendation_result.html')
+  
+def fish_information(request, *args, **kwargs):
+  return render(request, 'noviceAngler/fish_information.html')
+  
